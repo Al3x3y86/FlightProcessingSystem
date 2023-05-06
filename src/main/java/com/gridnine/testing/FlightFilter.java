8716fb6644ec4d0d5b802fac20b048ab1173e10b
Alexey <alexey_kuzminykh@mail.ru>
2023-05-07 01:44:55 +0300

    application code

---
+++ src/main/java/com/gridnine/testing/FlightFilter.java
@@ -0,0 +1,19 @@
+package com.gridnine.testing;
+
+import java.util.List;
+
+/**
+ * Interface for the FlightFilterImpl class
+ */
+
+public interface FlightFilter {
+
+    void allFlightSegments(List<Flight> flights);
+
+    List<Flight> DepartureToTheCurrentPointInTime(List<Flight> flights);
+
+    List<Flight> thereAreSegmentsWithTheArrivalDateEarlierThanTheDepartureDate(List<Flight> flights);
+
+    List<Flight> theTotalTimeSpentOnTheGroundExceedsTwoHours(List<Flight> flights);
+
+}
